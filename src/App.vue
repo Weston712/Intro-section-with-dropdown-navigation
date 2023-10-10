e864570b400ae4657f40550d63c6c203b7b66fb7
Weston712 <101402359+Weston712@users.noreply.github.com>
2023-10-10 22:57:37 +0300

    new project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import PageVue from "./components/Page.vue";
+
+export default {
+  components: {
+    PageVue,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <PageVue></PageVue>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Page.vue
@@ -0,0 +1,24 @@
+<script>
+import HeaderVue from "./Header.vue";
+
+export default {
+  components: {
+    HeaderVue,
+  },
+};
+</script>
+
+<template>
+  <div class="wrapper__container">
+    <HeaderVue></HeaderVue>
+  </div>
+</template>
+
+<style scoped>
+.wrapper__container {
+  width: 100%;
+  height: 100vh;
+  padding-top: 22px;
+  background-color: #fafafa;
+}
+</style>
